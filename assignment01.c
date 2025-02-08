bd8386327eda41ac5d6245b13d88fb6d52f7e577
Julius Muhizi <72916995+julio15-coder@users.noreply.github.com>
2025-02-08 16:31:00 +0000

    A program to add two numbers

---
+++ assignment01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;  // Declare variables
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);  // Take input
+
+    sum = a + b;  // Add numbers
+
+    printf("Sum = %d\n", sum);  // Print result
+
+    return 0;
+}
